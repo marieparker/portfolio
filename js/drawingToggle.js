171ada80cb651aafac8a3f00b5e6fff9b4d4fd65
marieparker <lmparker@byui.edu>
2020-07-21 22:53:29 -0700

    update

--- js/drawingToggle.js
+++ js/drawingToggle.js
@@ -53,3 +53,34 @@ function moveRight4() {
     document.getElementsByClassName("scroll")[1].src = "images/ArchitecturalProjectSingleStory" + picture + ".jpg";
     document.getElementsByClassName("scroll")[1].id = picture;
 }
+
+function moveRight8() {
+    
+    var picture = document.getElementsByClassName("scroll")[0].getAttribute('id');
+    picture = parseFloat(picture);
+
+    if (picture == 27) {
+        picture = 19;
+    }
+    var newID = picture + 1
+    picture = newID - 20;
+
+    document.getElementsByClassName("scroll")[0].src = "images/Flux-page-" + picture + ".jpg";
+    document.getElementsByClassName("scroll")[0].id = newID;
+}
+
+function moveLeft8() {
+    
+    var picture = document.getElementsByClassName("scroll")[0].getAttribute('id');
+    picture = parseFloat(picture);
+console.log(picture);
+    if (picture == 20) {
+        picture = 28;
+    }
+
+    var newID = picture - 1;
+    picture = newID - 20;
+console.log(newID);
+    document.getElementsByClassName("scroll")[0].src = "images/Flux-page-" + picture + ".jpg";
+    document.getElementsByClassName("scroll")[0].id = newID;
+}
