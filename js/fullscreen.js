24b58e35e2e234bdd36c11873489b3f9fb106139
marieparker <lmparker@byui.edu>
2020-07-29 17:28:28 -0700

    update

--- js/fullscreen.js
+++ js/fullscreen.js
@@ -1,4 +1,13 @@
 function fullScreen(clicked_id) {
+    var classes = document.getElementById(clicked_id).className;
+    if (classes = "align-bottom-cell") {
+        document.getElementById(clicked_id).classList.toggle("align-bottom-cell");
+    }
+    document.getElementById(clicked_id).classList.toggle("full-screen");
+    document.getElementById("body").classList.toggle("faded");
+}
+
+function fullScreen1(clicked_id) {
     document.getElementById(clicked_id).classList.toggle("full-screen");
     document.getElementById("body").classList.toggle("faded");
 }
